eceea666e62929c9a8b14c4bdc342fdf7e68aa38
Adam Silverstein <adam@10up.com>
2019-08-16 14:57:40 -0600

    update dist files

--- dist/index.js
+++ dist/index.js
@@ -12,3 +12,4 @@ var _CharacterMap2 = _interopRequireDefault(_CharacterMap);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.CharacterMap = _CharacterMap2.default;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6WyJDaGFyYWN0ZXJNYXAiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBQTs7Ozs7O1FBQ1NBLFksR0FBQUEsc0IiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgQ2hhcmFjdGVyTWFwIGZyb20gJy4vY29tcG9uZW50L0NoYXJhY3Rlck1hcCc7XG5leHBvcnQgeyBDaGFyYWN0ZXJNYXAgfTsiXX0=
